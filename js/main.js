b364296885776560b2b4751ce25b0ce050ccd717
David Rajchenbach-Teller <dteller@mozilla.com>
2013-11-21 14:19:36 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function() {
+  var $ = document.getElementById.bind(document);
+  var myConsole = $("console");
+  for (var id of ["pad_1", "pad_2", "pad_3", "pad_4"]) {
+    (function() {
+      var id2 = id;
+      var pad = $(id);
+      pad.addEventListener("touchmove", function() {
+        myConsole.textContent += "touchmove " + id2;
+        myConsole.innerHTML += "<br />";
+      });
+      pad.addEventListener("mousemove", function() {
+        myConsole.innerHTML += "mousemove " + id2 + "<br />";
+      });
+    })();
+  }
+})();
