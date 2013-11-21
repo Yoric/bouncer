06c4d73181f5e59a6a3c3b4eaebe0841c2df7104
David Rajchenbach-Teller <dteller@mozilla.com>
2013-11-21 20:47:42 +0100

    Issue #3: Pads can move, somewhat

--- js/main.js
+++ js/main.js
@@ -1,18 +1,34 @@
 (function() {
   var $ = document.getElementById.bind(document);
+
   var myConsole = $("console");
+  var touches = new Map();
+
   for (var id of ["pad_1", "pad_2", "pad_3", "pad_4"]) {
     (function() {
       var id2 = id;
-      var pad = $(id);
-      pad.addEventListener("touchmove", function() {
-        myConsole.innerHTML += "touchmove " + id2 + "<br />";
+      var elt = $(id);
+      var style = elt.style;
+      var transform = function(e) {
+        var result = "translateX(" + (e.pageX - 15) + "px) translateY(" + (e.pageY - 15) + "px)";
+        console.log(result);
+        return result;
+      };
+
+      // Touch-based control
+      elt.addEventListener("touchstart", function(e) {
       });
-/*
-      pad.addEventListener("mousemove", function() {
-        myConsole.innerHTML += "mousemove " + id2 + "<br />";
+      elt.addEventListener("touchmove", function(e) {
+        style.transform = transform(e);
       });
-*/
+
+      // Mouse-based control
+      elt.addEventListener("mousemove", function(e) {
+        style.transform = transform(e);
+      });
+      elt.addEventListener("mouseleave", function(e) {
+        style.transform = transform(e);
+      });
     })();
   }
 })();
