34b110455f36e55e4106ea9407f3a1372e341ea7
David Rajchenbach-Teller <dteller@mozilla.com>
2013-12-02 17:29:03 +0100

    Issue #3: We can now move the pads using the mouse

--- js/main.js
+++ js/main.js
@@ -1,34 +1,163 @@
-(function() {
+(function bouncer() {
   var $ = document.getElementById.bind(document);
 
-  var myConsole = $("console");
-  var touches = new Map();
+  function Sprite(id) {
+    this.id = id;
+    this.element = document.getElementById(id);
+    this.style = this.element.style;
 
-  for (var id of ["pad_1", "pad_2", "pad_3", "pad_4"]) {
+    // The position of the sprite, in pixels.
+    this._x = null;
+    this._y = null;
+
+    // The current destination of the sprite, in pixels.
+    this._nextX = this._x;
+    this._nextY = this._y;
+
+    this.width = null;
+    this.height = null;
+
+    this.event = {};
+    this.changed = false;
+
+    this.readFromDOM();
+  }
+  Sprite.prototype = {
+    get x() {
+      return this._x;
+    },
+    get y() {
+      return this._y;
+    },
+    set x(x) {
+      this.changed = true;
+      this._nextX = x;
+    },
+    set y(y) {
+      this.changed = true;
+      this._nextY = y;
+    },
+
+    /**
+     * Perform all reads from DOM
+     */
+    readFromDOM: function() {
+      this._x = this.element.offsetLeft;
+      this._y = this.element.offsetTop;
+      this.width = this.element.offsetWidth;
+      this.height = this.element.offsetHeight;
+      this.changed = false;
+    },
+
+    /**
+     * Write to the DOM the values of this.nextX, this.nextY
+     */
+    writeToDOM: function() {
+      if (!this.changed) {
+        return;
+      }
+      this.style.left = this._nextX + "px";
+      this.style.top  = this._nextY + "px";
+    }
+  };
+
+  var sprites = {
+    padNorth: new Sprite("pad_north"),
+    padSouth: new Sprite("pad_south"),
+    padEast: new Sprite("pad_east"),
+    padWest: new Sprite("pad_west")
+  };
+
+  // Set initial positions
+
+  var padNorth = sprites.padNorth;
+  var padSouth = sprites.padSouth;
+  var padEast = sprites.padEast;
+  var padWest = sprites.padWest;
+
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
+  padNorth.style.top = "0px";
+  padNorth.style.left = (width - padNorth.width) / 2 + "px";
+  padNorth.writeToDOM();
+
+  padSouth.style.bottom = "0px";
+  padSouth.style.left = (width - padSouth.width) / 2 + "px";
+  padSouth.writeToDOM();
+
+  padEast.style.top = (height - padEast.height) / 2 + "px";
+  padEast.style.right = "0px";
+  padEast.writeToDOM();
+
+  padWest.style.left = "0px";
+  padWest.style.bottom = (height - padWest.height) / 2 + "px";
+  padWest.writeToDOM();
+
+
+  for (var key of ["padNorth", "padSouth", "padEast", "padWest"]) {
     (function() {
-      var id2 = id;
-      var elt = $(id);
-      var style = elt.style;
-      var transform = function(e) {
-        var result = "translateX(" + (e.pageX - 15) + "px) translateY(" + (e.pageY - 15) + "px)";
-        console.log(result);
-        return result;
-      };
+      var sprite = sprites[key];
+      var elt = sprite.element;
+      var event = sprite.event;
 
       // Touch-based control
       elt.addEventListener("touchstart", function(e) {
       });
       elt.addEventListener("touchmove", function(e) {
-        style.transform = transform(e);
+        event.pageX = e.pageX;
+        event.pageY = e.pageY;
       });
 
       // Mouse-based control
-      elt.addEventListener("mousemove", function(e) {
-        style.transform = transform(e);
+      // (used for testing)
+      document.body.addEventListener("mousemove", function(e) {
+        event.pageX = e.pageX;
+        event.pageY = e.pageY;
       });
       elt.addEventListener("mouseleave", function(e) {
-        style.transform = transform(e);
       });
     })();
   }
+
+
+  var nextFrame = function() {
+    // Get the position of all items
+    // We need to do this *before* doing any change to CSS or position,
+    // for performance reasons.
+    for (var key of Object.keys(sprites)) {
+      sprites[key].readFromDOM();
+    }
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    // From this point on, there should be NO read from DOM.
+
+
+    // FIXME: Handle pause
+
+    // FIXME: Handle bounce
+
+    // FIXME: Handle pad movement
+    padNorth.x = padNorth.event.pageX;
+    padSouth.x = padSouth.event.pageX;
+    padEast.y = padEast.event.pageY;
+    padWest.y = padWest.event.pageY;
+
+    // FIXME: Handle ball movement
+
+    // FIXME: Handle health, win/lose
+
+    for (key of Object.keys(sprites)) {
+      sprites[key].writeToDOM();
+    }
+  };
+
+  nextFrame();
+
+  // Main loop
+  requestAnimationFrame(function loop() {
+    nextFrame();
+    requestAnimationFrame(loop);
+  });
 })();
